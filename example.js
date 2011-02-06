ea85ed4cac040bc19334fea9fe1b815c10560538
Thomas Sydorowski <thomas@tsyd.net>
2011-02-05 18:03:13 -0800

    Added onResponse handler.

--- example.js
+++ example.js
@@ -1,8 +1,29 @@
 var proxy = require('./lib/proxy-mock');
 
-proxy.start({port: 80}, function (p) {
+proxy.start({port: 8080}, function (p) {
   p.mock(/test/, 'mocked');
   p.mock(/translate/, function () {
     return '{"responseData": {"translatedText": "La la la."}}'
+  });
+
+  p.mock(/tsyd\.net/, function (request) {
+    request.onResponse(function (response) {
+      // called when we have the response from the mocked url
+      if (response.headers['content-type'] = 'text/html') {
+        var matches = response.data.match(/(<h\d>.*?<\/h\d>)/mg);
+
+        if (matches) {
+          // reverse the text within all header tags
+          matches.forEach(function (match) {
+            var parts = match.match(/(<h\d>)(.*?)(<\/h\d>)/);
+            response.data = response.data.replace(parts[0],
+              parts[1] + parts[2].split('').reverse().join('') + parts[3]);
+          });
+        }
+      }
+
+      // the onResponse handler must complete the response
+      response.complete();
+    });
   });
 });
